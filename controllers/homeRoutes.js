4a61710d6264ca41cb436d6c4ab1ee5ee934663b
markosanchez800 <markosanchez800@gmail.com>
2021-05-02 18:26:13 -0700

    login button redirects to login/register page and home button goes back to homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,19 +1,25 @@
 const router = require('express').Router();
 const User = require('../models/User')
+const Blog = require('../models/Blog')
 //const withAuth = require('../utils/auth');
 
 router.get('/', async (req,res)=>{
     try{
-        const userData = await User.findAll({
+        const blogData = await Blog.findAll({
         });
-        const users = userData.map((user) => user.get({plain: true}));
+        const blogs = blogData.map((blog) => blog.get({plain: true}));
+        console.log(blogs);
         res.render('homepage',{
-            users,
+            blog: blogs
             //logged_in: req.session.logged_in,
         });
-    } catch (err){
+    } catch (err) {
         res.status(500).json(err);
     }
 });
+
+router.get('/login', async (req,res)=>{
+   res.render('login');
+})
 
 module.exports = router;
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,7 +1,8 @@
 const sequelize = require('../config/connection');
 const User = require('../models/User')
-
+const Blog = require('../models/Blog')
 const userData = require('./userData.json');
+const blogData = require('./blogData.json');
 
 const seedDb = async () => {
     await sequelize.sync({ force: true });
@@ -10,6 +11,11 @@ const seedDb = async () => {
         individualHooks: true,
         returning: true,
     });
+
+    await Blog.bulkCreate(blogData,{
+        returning:true,
+    });
+
     process.exit(0);
 };
 
